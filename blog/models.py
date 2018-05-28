241ec5f639d23c022092c44cd624cb8645bde459
dlwpdlr <dlwpdlr@gmail.com>
2018-05-28 18:54:24 +0900

    error fix

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,6 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    photo = models.ImageField(blank=True)
     text = models.TextField()
     created_date = models.DateTimeField(
             default=timezone.now)
