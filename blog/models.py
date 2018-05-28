b3453962c96a1cf2eebc078aa2bcc2eee0223075
dlwpdlr <dlwpdlr@gmail.com>
2018-05-28 21:49:33 +0900

    image add

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
+    image = models.ImageField(blank=True)
+    #filtered_image = models.ImageField()
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
